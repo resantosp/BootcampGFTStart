501741b2fd313ed8cecfd15ddbeecd293db775ca
Renata Santos <renatados.santosp@gmail.com>
2022-02-02 18:03:31 -0300

    update aula tipos de ref

--- AulaRef/AulaRef/Program.cs
+++ AulaRef/AulaRef/Program.cs
@@ -13,17 +13,7 @@ class AulaRef
 
         System.Console.WriteLine(a); 
     }
-    static void TrocarNome(string[] nomes, string nomeNovo, string nomeAntigo)
-    {
-        for (int i = 0; i < nomes.Length; i++)
-        {
-            if (nomes[i] == nomeAntigo)
-            {
-                nomes[i] = nomeNovo;
-            }
-        }
-    }
-    static void Main()
+    static void Demo2()
     {
        var nomes = new string[]{"Renata", "Mariana", "Ricardo"};
 
@@ -36,10 +26,69 @@ class AulaRef
        System.Console.Write("Digite o nome a ser incluído: ");
        string nomeNovo = Console.ReadLine().Trim();
 
-       TrocarNome(nomes, nomeNovo, nomeAntigo);
+       //TrocarNome(nomes, nomeNovo, nomeAntigo);
 
+       System.Console.WriteLine();
        System.Console.WriteLine($@"A lista de nomes ALTERADA é:
        {string.Join(", ", nomes)}");
+
+    }
+    static void Demo3()
+    {
+        var nomes = new string[]{"Renata", "Mariana", "Ricardo"};
+
+       System.Console.WriteLine($@"A lista de nomes ATUAL é:
+       {string.Join(", ", nomes)}
+       ");
+
+       System.Console.Write("Digite o nome a ser substituído: ");
+       string nomeAntigo = Console.ReadLine().Trim();
+
+       int indice = LocalizarNome(nomes, nomeAntigo);
+
+       if (indice == -1)
+       {
+           System.Console.WriteLine("\nNome não encontrado na lista.");
+       }
+       else
+       {
+           System.Console.Write("Digite o nome a ser incluído: ");
+           string nomeNovo = Console.ReadLine().Trim();
+           
+           nomes[indice] = nomeNovo;
+           
+           System.Console.WriteLine();
+           System.Console.WriteLine($@"A lista de nomes ALTERADA é:
+           {string.Join(", ", nomes)}");
+       }
+    }
+    static int LocalizarNome(string[] nomes, string nomeAntigo)
+    {
+        for (int i = 0; i < nomes.Length; i++)
+        {
+            if (nomes[i] == nomeAntigo)
+            {
+                return i;
+            }
+        }
+        //Como usei um if, precisa dar um return aqui fora também para caso não encontre o nome
+        return -1;
+        
+    }
+    static void TrocarNome(string[] nomes, string nomeNovo, int indice)
+    {
+        for (int i = 0; i < nomes.Length; i++)
+        {
+            if (nomes[i] == nomes[indice])
+            {
+                nomes[i] = nomeNovo;
+            }
+        }
+    }
+    static void Main()
+    {
+       
+       
 
     }
 }
